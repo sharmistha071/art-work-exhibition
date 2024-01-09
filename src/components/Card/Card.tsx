87b05ad204e635395e6dfa3768480fb33a8c4fc3
sharmistha071 <shormy.roy@gmail.com>
2024-01-09 14:26:24 +0000

    card component created

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
-import "./App.css";
+import "./Card.css";
 
-const Card = () => {
-  const [count, setCount] = useState(0);
-
+const Card = ({ content }) => {
+  console.log("item,", content);
   return (
-    <div>
-      <p>hello</p>
+    <div className="card">
+      <img
+        src={
+          content.cover_id
+            ? "https://covers.openlibrary.org/b/id/" +
+              content.cover_id +
+              "-L.jpg"
+            : "https://dummyimage.com/180x190/dedede/3b3b3b&text=Image+Not+Available"
+        }
+        alt="Avatar"
+      />
+      <div className="container">
+        <h2>{content.title}</h2>
+        <h4>
+          <b>{content.author_name}</b>
+        </h4>
+        <p className="content">{content.first_sentence}</p>
+      </div>
     </div>
   );
 };
