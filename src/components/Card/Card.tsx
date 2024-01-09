9de3929b51da685a0401e2829a850905fc224a75
sharmistha071 <shormy.roy@gmail.com>
2024-01-09 21:50:12 +0000

    Changes in card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,23 +1,24 @@
 import "./Card.css";
+import { FormattedArtWork } from "../../utils/globalTypes";
 
-const Card = ({ content }) => {
-  console.log("content", content);
+type CardProps = {
+  content: FormattedArtWork;
+};
+
+const Card = ({ content }: CardProps) => {
   return (
     <div className="card">
       <img
         src={
-          content.cover_id
-            ? "https://covers.openlibrary.org/b/id/" +
-              content.cover_id +
-              "-L.jpg"
+          content.thumbnail
+            ? `https://www.artic.edu/iiif/2/${content.thumbnail}/full/274,263/0/default.jpg`
             : "https://dummyimage.com/180x190/dedede/3b3b3b&text=Image+Not+Available"
         }
         alt="Avatar"
       />
       <div className="container">
-        <h2>{content.title}</h2>
-        <h4>{content.author_name}</h4>
-        <p className="content">{content.first_sentence}</p>
+        <h4 className="title">{content.title}</h4>
+        <p className="content">{content.alt_text}</p>
       </div>
     </div>
   );
